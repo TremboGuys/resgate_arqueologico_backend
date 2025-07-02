d1bf7ae6cc9313987e6c8a05de7997bd1783601c
Joaovictor23t4 <joaovictor239090@gmail.com>
2025-07-01 21:02:39 -0300

    FEAT: Create API for Quiz #3

--- core/models/classroom.py
+++ core/models/classroom.py
@@ -1,4 +1,7 @@
 from django.db import models
 
 class Classroom(models.Model):
-    nome = models.CharField(max_length=7, unique=True)
+    name = models.CharField(max_length=7, unique=True)
+
+    def __str__(self):
+        return self.name
